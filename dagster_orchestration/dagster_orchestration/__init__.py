9631392a79799716521a16b992a427154cd5e5c1
Jha <Pranav.Jha@fujitsu.com>
2024-07-10 15:44:30 -0400

    added dagster

--- dagster_orchestration/dagster_orchestration/__init__.py
+++ dagster_orchestration/dagster_orchestration/__init__.py
@@ -1,9 +1,14 @@
-from dagster import Definitions, load_assets_from_modules
+import os
+from dagster_dbt import DbtCliClientResource
+from dagster_dbt import load_assets_from_dbt_project
 
-from . import assets
+resources = {
+    "dbt": DbtCliClientResource(
+        project_dir=os.getenv("DBT_PROJECT_DIR"),
+        profiles_dir=os.getenv("DBT_PROFILES_DIR"),
+    ),
+}
 
-all_assets = load_assets_from_modules([assets])
-
-defs = Definitions(
-    assets=all_assets,
+dbt_assets = load_assets_from_dbt_project(
+    project_dir=os.getenv("DBT_PROJECT_DIR"), profiles_dir=os.getenv("DBT_PROFILES_DIR"), key_prefix=["transformed_data"]
 )
